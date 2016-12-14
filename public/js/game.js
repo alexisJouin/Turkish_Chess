4cb0f43f26686fbf64c22f870f468eb4c2c95b9a
Jouin Alexis <alexis.jouin@live.fr>
2016-12-14 13:43:41 +0100

    ADD : plateau get valeur du moteur Refactoring dans game.js TODO move des pions

--- public/js/game.js
+++ public/js/game.js
@@ -1,65 +1,93 @@
-$( document ).ready(function() {
+var socket = io.connect('http://localhost:3000');
+socket.emit('play');
 
-   var board = Array(
-       Array("","","","","","","",""),
-       Array("w","w","w","w","w","w","w","w"),
-       Array("w","w","w","w","w","w","w","w"),
-       Array("","","","","","","",""),
-       Array("","","","","","","",""),
-       Array("b","b","b","b","b","b","b","b"),
-       Array("b","b","b","b","b","b","b","b"),
-       Array("","","","","","","","")
-   );
+//On get le plateau du moteur
+socket.on('board', function (board) {
+    constructBoard(board);
+});
 
-    var output="" +
+
+$(document).on('click', 'img', function () {
+    movePiece($(this));
+});
+
+/** List of functions **/
+
+/**
+ * Construction du plateau dans le view
+ * @param board => tableau de 8 tableaux contenant les pions
+ * 0 => vide / 1 => Noir / 2 => Blanc
+ */
+function constructBoard(board) {
+    var output = "" +
         "<thead>" +
         "<tr><th><th>A<th>B<th>C<th>D<th>E<th>F<th>G<th>H" +
         "<tbody>";
-    var nbLigne = 1;
+    var nbLigne = 8;
+    board.forEach(function (ligne) {
+        output += "<tr class='row'><th>" + nbLigne;
+        nbLigne--;
+        ligne.forEach(function (element) {
 
-    console.log(board);
-
-    board.forEach(function(ligne){
-        output += "<tr class='row'><th>" + nbLigne ;
-        nbLigne++;
-        ligne.forEach(function(element) {
-
-            if(element=="b"){
+            //Blanc
+            if (element == 1) {
                 //&#9823;
-               output += "<td class='tile'><img src='img/noirPion.png' class='blackPawn'/></td>";
+                output += "<td class='tile'><img src='img/noirPion.png' class='piece blackPawn'/></td>";
             }
-            else if(element =="w"){
+            else if (element == 2) {
                 //&#9817;
-                output += "<td class='tile'><img src='img/blancPion.png' class='whitePawn'/></td>";
+                output += "<td class='tile'><img src='img/blancPion.png' class='piece whitePawn'/></td>";
             }
-            else{
+            else {
                 output += "<td class='tile'></td>";
             }
 
-
-
-            console.log(element);
         });
     });
-
     $('#board').children('table').append(output);
-    $('img').draggable({
-        containment: "parent"
+}
+
+/**
+ * Déplacement d'un pion
+ * @param element => pion sélectionné
+ */
+function movePiece(element) {
+    var plateau = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    
+    $('.piece').removeClass('selectedPiece');
+    element.addClass('selectedPiece');
+
+    element.draggable({
+        containment: "",
+        //stack: '#board table',
+        revert: true
     });
+
+
+    ui.draggable.addClass( 'correct' );
+    ui.draggable.draggable( 'disable' );
+    $(this).droppable( 'disable' );
+    ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+    ui.draggable.draggable( 'option', 'revert', false );
+    correctCards++;
+
+
     $('h1').droppable({
-        drop: function( event, ui ) {
-            $( this )
-                .addClass( "ui-state-highlight" )
-                .find( "p" )
-                .html( "Dropped!" );
+        drop: function (event, ui) {
+            $(this)
+                .addClass("ui-state-highlight")
+                .find("p")
+                .html("Dropped!");
         }
     });
-
-    $('img').click(function(){
-        $('img').css('border', 'none');
-        $(this).css('border', '2px solid green');
-        $(this).css('border-radius', '180px');
-    });
-
-
-});
+}
