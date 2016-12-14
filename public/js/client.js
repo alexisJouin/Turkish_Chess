8662242191ad9c78953f8779ea67dfbe5e45aaa6
Sébastien <schouteeten.sebastien@gmail.com>
2016-12-14 15:22:33 +0100

    ADD - Players and turns management with sockets

--- public/js/client.js
+++ public/js/client.js
@@ -1,27 +1,24 @@
-/**
- * Created by Harinck Aurelien on 08/06/2016.
- */
 var socket = io.connect('http://localhost:3000');
-
 
 $('#play').click(function () {
     socket.emit('play');
 });
 
+$('#move').click(function () {
+    socket.emit('move', []);
+});
+
 socket.on('board',function(board) {
     console.log(board);
 });
-/*
-socket.on('test', function(message){
-    alert(message);
-});
-*/
+
+$('#move').attr('disabled','disabled');
 socket.on('turn',function(turn) {
     if(turn){
-        console.log("C'est votre tour");
-        socket.emit("move", [12,5]);
+        $('#move').removeAttr('disabled');
     }
     else {
-        console.log("Ce n'est pas votre tour");
+        $('#move').attr('disabled','disabled');
     }
 });
+
