d26ab740a3cb78b998e053b214490dfccf9dc350
sifatekeli <sifatkl62@gmail.com>
2016-12-15 09:20:09 +0100

    ADD & CHANGE

--- routes/menu.js
+++ routes/menu.js
@@ -12,18 +12,77 @@ router.get('/', function(req, res, next) {
     if(!req.session._id && !req.session.username && !req.session.mail){  // Si non connecte, redirige vers connexion
         res.redirect('/connexion');
     }else { // Sinon on affiche menu
-        /*
-        var newGame = {
-            id_player1: req.session._id,
-            id_player2: "58500078f96faa5c1db01b6c",
-            num_winner: 2,
-            number_of_turns: 12
-        };
+        // Creation classement
 
-        req.app.db.models.Game.create(newGame, function (err, game) {
 
-        });
-        */
+        tab_classement = [];
+        var i = 0;
+        req.app.db.models.User.find(function(enn,users){
+            users.forEach(function(user){
+                tab_classement[i] = []
+                tab_classement[i]['rang'] = i+1;
+                tab_classement[i]['pseudo'] = user.username;
+                tab_classement[i]['points'] = user.points;
+                i++;
+
+
+            });
+            // Compte le nombre de parties d'un joueur
+            req.app.db.models.Game.count( { $or: [ { 'player1':req.session.username }, { 'player2':req.session.username } ] }, function(err, req_number_of_games){
+                if(req_number_of_games > 0) {
+                    // S'il a au moins une partie, on la/les selectionnes
+                    req.app.db.models.Game.find({$or: [{'player1': req.session.username}, {'player2': req.session.username}]}, function (err, req_games) {
+                        stats_game = [];
+                        stats_game['nbmatch'] = req_number_of_games;
+                        stats_game['nbwin'] = 0;
+                        stats_game['nbturnmin'] = req_games[0].number_of_turns;
+                        var number_of_turn = 0;
+
+                        for(var i = 0; i < req_number_of_games; i++){
+                            if(req_games[i].winner == req.session.username){
+                                stats_game['nbwin']++;
+                                if(req_games[i].number_of_turns < stats_game['nbturnmin']){
+                                    stats_game['nbturnmin'] = req_games[i].number_of_turns;
+                                }
+                            }
+                            number_of_turn += req_games[i].number_of_turns;
+
+                        }
+
+                        stats_game['ratio'] = Math.round((stats_game['nbwin'] / stats_game['nbmatch'])*100)/100;
+                        stats_game['nbturnmean'] = Math.round((number_of_turn / stats_game['nbmatch'])*100)/100;
+
+
+
+                        res.render('menu', {
+                            _id: req.session._id,
+                            username: req.session.username,
+                            mail: req.session.mail,
+                            has_game: 1,
+                            stats: stats_game,
+                            classement: tab_classement
+                        });
+
+                    });
+
+                }else{
+                    res.render('menu', {
+                        _id: req.session._id,
+                        username: req.session.username,
+                        has_game: 0,
+                        mail: req.session.mail,
+                        classement: tab_classement
+                    });
+                }
+
+            });
+        }).sort( { points: -1 } );
+
+
+
+
+
+/*
 
         // Compte le nombre de parties d'un joueur
         req.app.db.models.Game.count( { $or: [ { 'player1':req.session.username }, { 'player2':req.session.username } ] }, function(err, req_number_of_games){
@@ -50,8 +109,6 @@ router.get('/', function(req, res, next) {
                     stats_game['ratio'] = Math.round((stats_game['nbwin'] / stats_game['nbmatch'])*100)/100;
                     stats_game['nbturnmean'] = Math.round((number_of_turn / stats_game['nbmatch'])*100)/100;
 
-
-                    console.log(stats_game);
                     res.render('menu', {
                         _id: req.session._id,
                         username: req.session.username,
@@ -72,7 +129,7 @@ router.get('/', function(req, res, next) {
             }
 
         });
-
+*/
 
 
     }
@@ -88,7 +145,28 @@ router.get('/deconnexion', function(req, res, next) {
 
 });
 
+/*
+function getRatio(pseudo) {
+    req.app.db.models.Game.count({$or: [{'player1': pseudo}, {'player2': pseudo}]}, function (err, req_number_of_games) {
+        if(req_number_of_games == 0){
+            return 0;
+        }
+        var number_of_match = 0;
+        var number_of_win = 0;
+        req.app.db.models.Game.find({$or: [{'player1': pseudo}, {'player2': pseudo}]}, function (err, req_games) {
 
+            for(var i = 0; i < req_number_of_games; i++){
+                if(req_games[i].winner == pseudo) {
+                    number_of_win = number_of_win + 1;
+                }
+                number_of_match = number_of_match +1;
+            }
+        });
+        return number_of_win/number_of_match;
+
+    });
+}
+*/
 
 module.exports = router;
 
