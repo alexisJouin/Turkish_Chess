845e59ce10e971d3ade7e2a94e7a28cf1c357c07
Clément Rémond <cremond@pechel.weisshorn.calais.fr>
2016-12-15 10:44:38 +0100

    Hot fix ;)

--- core/board.js
+++ core/board.js
@@ -24,6 +24,7 @@ module.exports = (function (self) {
             // Init player
 //            players.push(1);
 //            players.push(2);
+            playingPlayerIndex = 0;
 
             // Init pawns
             for (var i = 0; i < 16; i++) {
