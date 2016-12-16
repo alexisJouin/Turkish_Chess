815c9b2072dfc15744e88fd4f2d80527881bf997
esailly <esailly@free.fr>
2016-12-16 10:27:43 +0100

    fix  test attack : it's fail

--- test/ScenarioTestPlay.js
+++ test/ScenarioTestPlay.js
@@ -25,7 +25,14 @@ var myColor = function (_board, posLine, posColumn) {
         var pawn = _board.getPositionBoard(posLine, posColumn);
         return  pawn.getColour();
     } else return  "Empty";
-}
+};
+
+var myIsQueen = function (_board, posLine, posColumn) {
+    if (!_board.empty([posLine, posColumn])) {
+        var pawn = _board.getPositionBoard(posLine, posColumn);
+        return  pawn.isQueen();
+    } else return  false;
+};
 
 describe('Test a Play', function () {
     var board = new core.Board();
@@ -60,6 +67,7 @@ describe('Test a Play', function () {
         expect(board.empty([2,3])).equal(true);
         expect(board.empty([3,3])).equal(false);
         expect(myColor(board,3,3)).equal("WHITE");
+        console.log(board.getBoardArray(),"\n");
 
         //possible but not authorize
         expect(board.getPossibleMoves(3,3)).to.deep.equal([ [3,2],[3,4],[4,3] ]);
@@ -69,29 +77,39 @@ describe('Test a Play', function () {
         //player 2
 
         board.moveOrAttackPawn(5,3,4,3);
+        console.log(board.getBoardArray(),"\n");
 
         board.swapPlayer();
         //player 1
 
         //possibleAttack
         //todo attention multiple attack must be [5,5] and after [7,5] and Queen !
-        expect(ArrayMoveToString(board.getPossibleAttacks(3,3))).to.deep.equal("[ [5,5] ]");
-       // expect(ArrayMoveToString(board.getPossibleAttacks(3,3))).to.deep.equal("[ [7,5] ]");
+        //expect(ArrayMoveToString(board.getPossibleAttacks(3,3))).to.deep.equal("[ [5,5] ]");
+        expect(ArrayMoveToString(board.getPossibleAttacks(3,3))).to.deep.equal("[ [7,3] ]");
         expect(ArrayMoveToString(board.getPossibleAttacks(2,2))).to.deep.equal("[]");
 
         //Attack
-        board.moveOrAttackPawn(3,3,5,3); //normaly must be (3,3,7,3)
+        board.moveOrAttackPawn(3,3,7,3); //normaly must be (3,3,7,3)
         expect(board.empty([3,3])).equal(true); // White origin
+
         //todo remove black after attack
-        //expect(board.getNbPawns("BLACK")).equal(15);
-        //expect(board.empty([4,3])).equal(true); // Black to remove
-        expect(board.empty([5,3])).equal(false); // white final
+        expect(board.getNbPawns("BLACK")).equal(15);
+        expect(board.empty([4,3])).equal(true); // Black to remove
+        expect(board.empty([7,3])).equal(false); // white final
 
         console.log("Erreur black is always at [4,3]\n",board.getBoardArray(),"\n");
-        expect(myColor(board,5,3)).equal("WHITE");
+        // todo remove after debug
+      //  board.removePawn(4,3);
+      //  expect(board.empty([4,3])).equal(true);
+      //  console.log(board.getBoardArray(),"\n");
 
-        expect(board.getPossibleMoves(5,3)).equal(null);
-        expect(ArrayMoveToString(board.getPossibleAttacks(5,3))).to.deep.equal("[ [7,5] ]");
+
+        expect(myColor(board,7,3)).equal("WHITE");
+
+        // todo test if isqueen
+      //  expect(board.getPossibleMoves(5,3)).equal(null);
+      //  expect(ArrayMoveToString(board.getPossibleAttacks(5,3))).to.deep.equal("[ [7,3] ]");
+
 
         board.swapPlayer();
         //player 2
@@ -103,8 +121,45 @@ describe('Test a Play', function () {
 
 
 
+
+
     });
 
 
 
+});
+
+describe('Test a Win', function () {
+    var board = new core.Board();
+    var pawn;
+
+    it('New play for win', function () {
+
+        for (var i = 0 ; i < 8; i++) {
+            if (i< 3 || i > 4) {
+                board.removePawn(1,i);
+                board.removePawn(2,i);
+                board.removePawn(5,i);
+                board.removePawn(6,i);
+            }
+            if (i == 4) {
+                board.removePawn(1,i);
+                board.removePawn(2,i);
+            }
+        }
+        board.moveOrAttackPawn(2,3,3,3);
+        board.moveOrAttackPawn(3,3,4,3);
+        board.swapPlayer();
+        //player 2
+        board.moveOrAttackPawn(5,3,3,3);
+        //todo remove
+        board.removePawn(4,3);
+        console.log(board.getBoardArray());
+        expect(myIsQueen(board,1,3)).equal(true);
+
+
+
+
+
+    });
 });
